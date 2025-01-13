f10d78611832e05fa840472c9683ce572bf69c6c
VLMADev <angrucat@gmail.com>
2025-01-13 09:51:14 +0300

    add composer

---
+++ browsmaster/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '642af4dae8d254d8a16c2df15dbc01bcd0910721',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '642af4dae8d254d8a16c2df15dbc01bcd0910721',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
